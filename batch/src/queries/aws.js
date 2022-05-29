b5bb3a683c8d3872776d9ad2732a66a221d639c8
cjsrkd3321 <7424069@gmail.com>
2022-05-29 14:41:02 +0900

    Update ec2 query for adding vpc_id column

--- batch/src/queries/aws.js
+++ batch/src/queries/aws.js
@@ -7,12 +7,56 @@ export const SG_SERVER_EC2 = `
     private_ip_address as private_ip, 
     public_ip_address as public_ip, 
     instance_state as state, 
+    vpc_id,
     security_groups as sg
   FROM 
     aws_ec2_instance
   WHERE
     instance_state = 'running'
 `;
+
+// FIXME: LB 관련 쿼리 수정 필요
+// export const SG_LB_NLB = `
+//   WITH records AS (
+//     SELECT RECORD.name as name, RECORD.type as type, RECORD.records ->> 0 as domain
+//     FROM aws_route53_zone as ZONE
+//     JOIN aws_route53_record AS RECORD ON ZONE.id = RECORD.zone_id
+//     WHERE
+//         RECORD.type = 'CNAME' or RECORD.type = 'A'
+//   )
+//   SELECT NLB.account_id, NLB.region, NLB.name, NLB.dns_name as record, records.name as dns_name, NLB.state_code
+//   FROM aws_ec2_network_load_balancer as NLB
+//     JOIN records ON records.domain = NLB.dns_name
+//   WHERE NLB.state_code = 'active'
+
+//   UNION
+
+//   SELECT NLB.account_id, NLB.region, NLB.name, NLB.dns_name as record, records.name as dns_name, NLB.state_code
+//   FROM aws_ec2_network_load_balancer as NLB
+//     LEFT OUTER JOIN records ON records.domain = NLB.dns_name
+//   WHERE NLB.state_code = 'active'
+// `;
+
+// export const SG_LB_ALB = `
+//   WITH records AS (
+//     SELECT RECORD.name as name, RECORD.type as type, RECORD.records ->> 0 as domain
+//     FROM aws_route53_zone as ZONE
+//     JOIN aws_route53_record AS RECORD ON ZONE.id = RECORD.zone_id
+//     WHERE
+//         RECORD.type = 'CNAME' or RECORD.type = 'A'
+//   )
+//   SELECT ALB.account_id, ALB.region, ALB.name, ALB.dns_name as record, records.name as dns_name, ALB.state_code
+//   FROM aws_ec2_application_load_balancer as ALB
+//     JOIN records ON records.domain = ALB.dns_name
+//   WHERE ALB.state_code = 'active'
+
+//   UNION
+
+//   SELECT ALB.account_id, ALB.region, ALB.name, ALB.dns_name as record, records.name as dns_name, ALB.state_code
+//   FROM aws_ec2_application_load_balancer as ALB
+//     LEFT OUTER JOIN records ON records.domain = ALB.dns_name
+//   WHERE ALB.state_code = 'active'
+// `;
 
 // // Account
 
@@ -43,15 +87,15 @@ export const SG_SERVER_EC2 = `
 //     group_name,
 //     security_group_rule_id,
 //     ip_protocol as protocol,
-//     CASE 
+//     CASE
 //       WHEN cidr_ip IS NULL
 //       THEN cidr_ipv6
 //       ELSE cidr_ip
 //       END AS ip,
-//     CASE 
-//       WHEN from_port = to_port 
-//       THEN from_port::varchar 
-//       ELSE from_port::varchar || '-' || to_port::varchar 
+//     CASE
+//       WHEN from_port = to_port
+//       THEN from_port::varchar
+//       ELSE from_port::varchar || '-' || to_port::varchar
 //       END AS port
 //   FROM
 //     aws_vpc_security_group_rule
